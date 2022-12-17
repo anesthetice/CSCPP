d034c65c98c596df250ef11884898350cc9f70f7
anesthetice <elliot.currat@epfl.ch>
2022-12-17 21:57:34 +0100

    finished cscpp

--- cscpp.sh
+++ cscpp.sh
@@ -1,71 +1,85 @@
 #!/bin/bash
 
+
 function cscpp {
-    GREEN='\033[1;32m'
-    NC='\033[0m'
-    if [ $# -gt 0 ]
-    then
-        echo -e -n "${GREEN}   Compiling${NC} '"$1"'"
-        if [ $# -gt 1 ]
-        then
-            echo -n " --> linked with : "
-            for ARGUMENT in $@
-            do
-                if [ $ARGUMENT != $1 ]
-                then
-                    echo -n "'"$ARGUMENT"' "
-                fi
-            done
-        fi
-        echo ""
-        g++ -std=c++11 -Wall "$@" -o "$1".out && \
-        echo -e "${GREEN}     Running${NC} '"$1".out'" && \
-        ./"$1".out
-    else
-    echo -e "cscpp \"g++ -std=c++11 -Wall\""
-    echo "Usage :"
-    echo "  * cscpp [FILE]"
-    echo "  * cscpp [MAIN_FILE] [FILE2] [FILE3] if your main c++ file is dependent on others"
-    echo "csccp clean"
-    fi
-}
-
-function newcscpp {
-    GREEN='\033[1;32m'
+    BRed='\033[1;31m'
+    BGreen='\033[1;32m'
     NC='\033[0m'
 
-    if ! [ $# -gt 0 ]
+    if ! [[ $# -gt 0 ]]
     then 
         echo "CSCPP Version 1.0"
         echo "use 'cscpp --help' for more information"
         return
     fi
 
-    if [ -e "$1" ]
+    if [[ "$1" == "-h" || "$1" == "--help" ]]
     then
-        echo -e -n "${GREEN}   Compiling${NC} '"$1"'"
-        if [ $# -gt 1 ]
+        echo "CSCPP Version 1.0"
+        echo ""
+        echo "Usage : cscpp [OPTIONS] [SUBCOMMAND]"
+        echo ""
+        echo "OPTIONS:"
+        echo "      -h, --help"
+        echo ""
+        echo "SUBCOMMANDS:"
+        echo "      build, b    compiles but does not execute a .cpp or .cc file"
+        echo "      run, r      default subcommand, compiles and executes a .cpp or .cc file"
+        echo "      clean       finds and removes all *.cpp.out in the current directory and sub-directories"
+        return
+    fi
+
+    if [[ -e "$1" ]]
+    then
+        echo -e -n "${BGreen}   Compiling${NC} '"$1"'"
+        if [[ $# -gt 1 ]]
         then
             echo -n " --> linked with : "
-            for ARGUMENT in $@
+            for ARGUMENT in {$@:2}
             do
-                if [ $ARGUMENT != $1 ]
-                then
-                    echo -n "'"$ARGUMENT"' "
-                fi
+                echo -n "'"$ARGUMENT"' "
             done
         fi
         echo ""
         g++ -std=c++11 -Wall "$@" -o "$1".out && \
-        echo -e "${GREEN}     Running${NC} '"$1".out'" && \
+        echo -e "${BGreen}     Running${NC} '"$1".out'" && \
         ./"$1".out
-    fi
-
     
-    if [ $1 == "clean" ]
+    elif [[ "$1" == "run" || "$1" == "r" ]]
+    then
+        echo -e -n "${BGreen}   Compiling${NC} '"$2"'"
+        if [[ $# -gt 2 ]]
+        then
+            echo -n " --> linked with : "
+            for ARGUMENT in ${@:3}
+            do
+                echo -n "'"$ARGUMENT"' "
+            done
+        fi
+        echo ""
+        g++ -std=c++11 -Wall "${@:2}" -o "$2".out && \
+        echo -e "${BGreen}     Running${NC} '"$2".out'" && \
+        ./"$2".out
+    
+    elif [[ "$1" == "build" || "$1" == "b" ]]
+    then
+        echo -e -n "${BGreen}   Compiling${NC} '"$2"'"
+        if [[ $# -gt 2 ]]
+        then
+            echo -n " --> linked with : "
+            for ARGUMENT in ${@:3}
+            do
+                echo -n "'"$ARGUMENT"' "
+            done
+        fi
+        echo ""
+        g++ -std=c++11 -Wall "${@:2}" -o "$2".out
+        
+
+    elif [[ $1 == "clean" ]]
     then
         FILE_LIST=($(find . -type f -name '*.cpp.out' -print))
-        if [ "$FILE_LIST" = "" ]
+        if [[ "$FILE_LIST" = "" ]]
         then
             echo "There is nothing to clean"
             return
@@ -76,8 +90,7 @@ function newcscpp {
             FILE_LIST_SIZE=$(("$FILE_LIST_SIZE" + 1))
             echo ">> "$ELEMENT""
         done
-        echo ""
-        read -p "confirm deletion? (y/n) " RESPONSE
+        read -p "Confirm deletion? (y/n) " RESPONSE
         if [[ "$RESPONSE" == "y" || "$RESPONSE" == "Y" || "$RESPONSE" == "yes" || "$RESPONSE" == "Yes" ]]
         then
             rm --interactive='never' "${FILE_LIST[@]}" 
@@ -86,31 +99,20 @@ function newcscpp {
             echo "Operation cancelled"
         fi
         return
-    fi
-
-
-
     
-
-
-
-<<comment
-    echo "CSCPP Version 1.0"
-    echo ""
-    echo "Usage : cscpp [OPTIONS] [SUBCOMMAND]"
-    echo ""
-    echo "OPTIONS:"
-    echo "      -h, --help"
-    echo ""
-    echo "SUBCOMMANDS:"
-    echo "      build, b    compiles but does not execute a .cpp or .cc file"
-    echo "      run, r      default subcommand, compiles and executes a .cpp or .cc file"
-    echo "      clean       finds and removes all *.cpp.out in the current directory and sub-directories"
-comment
+    else
+        echo -e "${BRed}error${NC} invalid subcommand and or argument"
+        echo "try 'cscpp --help' for more information"
+    fi
 }
 
-newcscpp solotest.cpp
-newcscpp test.cpp test2.cpp test3.cpp
-newcscpp solotest2.cpp
-newcscpp solotest3.cpp
-newcscpp clean
+cscpp run solotest.cpp
+cscpp r test.cpp test2.cpp test3.cpp
+cscpp build solotest2.cpp
+cscpp b solotest3.cpp
+cscpp clean
+cscpp
+cscpp -h
+
+echo 
+cscpp adhj
